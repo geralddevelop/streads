d449c60d2c2d6ddfb6afdc427d10f7a88ba0e7ff
Gerald <geralddevelop@gmail.com>
2024-06-08 02:03:39 +0800

    fix: to be more like figma

--- contents/productCatalogQuick.tsx
+++ contents/productCatalogQuick.tsx
@@ -1,5 +1,6 @@
 // This file show quick environmental details at the bottom of the product catalog page in shein.com.
 
+import { classNames } from "@utils/constants"
 import cssText from "data-text:~_styles/style.css"
 import type { PlasmoCSConfig, PlasmoGetInlineAnchorList } from "plasmo"
 
@@ -18,9 +19,31 @@ export const getInlineAnchorList: PlasmoGetInlineAnchorList = () =>
 
 const ProductCatalogQuick = () => {
   return (
-    <div className="bg-green-700 p-2 rounded-md w-full h-12 flex justify-center items-center z-10">
-        {/* ! No idea why but when the text is not text-sm, not showing */}
-      <p className="text-white text-sm text-center align-middle">Some environment information</p>
+    <div className="grid grid-cols-3 gap-2">
+      <QuickItem title="Chemical Free" status="good" />
+      <QuickItem title="No Eco Packaging" status="bad" />
+      <QuickItem title="13.66KG C02" status="bad" />
+    </div>
+  )
+}
+
+const QuickItem = ({
+  title,
+  status
+}: {
+  title: string
+  status: "good" | "bad"
+}) => {
+  return (
+    <div
+      className={classNames(
+        status === "good"
+          ? "border-green-800 text-green-800"
+          : "border-red-800 text-red-800",
+        "p-2 rounded-3xl w-full h-12 flex justify-center items-center z-10 border-green-800 border-2"
+      )}>
+      {/* ! No idea why but when the text is not text-sm, not showing */}
+      <p className="text-sm text-center align-middle">{title}</p>
     </div>
   )
 }
